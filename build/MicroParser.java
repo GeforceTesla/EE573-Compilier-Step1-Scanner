08c2de414e92b0cc3ed21c1ed3955638702f0557
Ying Yu Liu <liu282@ecegrid-thin3.ecn.purdue.edu>
2014-09-03 15:59:34 -0400

    initial draft for EE573 step 1

---
+++ build/MicroParser.java
@@ -0,0 +1,101 @@
+// Generated from Micro.g4 by ANTLR 4.1
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class MicroParser extends Parser {
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__17=1, T__16=2, T__15=3, T__14=4, T__13=5, T__12=6, T__11=7, T__10=8, 
+		T__9=9, T__8=10, T__7=11, T__6=12, T__5=13, T__4=14, T__3=15, T__2=16, 
+		T__1=17, T__0=18, OPERATOR=19, INTLITERAL=20, FLOATLITERAL=21, STRINGLITERAL=22, 
+		COMMENT=23, IDENTIFIERS=24, WS=25;
+	public static final String[] tokenNames = {
+		"<INVALID>", "'WRITE'", "'PROGRAM'", "'WHILE'", "'READ'", "'END'", "'FUNCTION'", 
+		"'CONTINUE'", "'ELSE'", "'ENDIF'", "'VOID'", "'BREAK'", "'FLOAT'", "'STRING'", 
+		"'IF'", "'RETURN'", "'BEGIN'", "'INT'", "'ENDWHILE'", "OPERATOR", "INTLITERAL", 
+		"FLOATLITERAL", "STRINGLITERAL", "COMMENT", "IDENTIFIERS", "WS"
+	};
+	public static final int
+		RULE_keyword = 0;
+	public static final String[] ruleNames = {
+		"keyword"
+	};
+
+	@Override
+	public String getGrammarFileName() { return "Micro.g4"; }
+
+	@Override
+	public String[] getTokenNames() { return tokenNames; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public MicroParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+	public static class KeywordContext extends ParserRuleContext {
+		public KeywordContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_keyword; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof MicroListener ) ((MicroListener)listener).enterKeyword(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof MicroListener ) ((MicroListener)listener).exitKeyword(this);
+		}
+	}
+
+	public final KeywordContext keyword() throws RecognitionException {
+		KeywordContext _localctx = new KeywordContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_keyword);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(2);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << 1) | (1L << 2) | (1L << 3) | (1L << 4) | (1L << 5) | (1L << 6) | (1L << 7) | (1L << 8) | (1L << 9) | (1L << 10) | (1L << 11) | (1L << 12) | (1L << 13) | (1L << 14) | (1L << 15) | (1L << 16) | (1L << 17) | (1L << 18))) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			consume();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\uacf5\uee8c\u4f5d\u8b0d\u4a45\u78bd\u1b2f\u3378\3\33\7\4\2\t\2\3\2"+
+		"\3\2\3\2\2\3\2\2\3\3\2\3\24\5\2\4\3\2\2\2\4\5\t\2\2\2\5\3\3\2\2\2\2";
+	public static final ATN _ATN =
+		ATNSimulator.deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
